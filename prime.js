4a1c920c7f37cf1a0cdd0bda5fa1791e606bcf07
Max <65962153+Blue-Sea2@users.noreply.github.com>
2021-05-05 20:38:17 +0200

    Update prime.js

--- prime.js
+++ prime.js
@@ -1,5 +1,5 @@
 console.log('%c This is the prime number checker ', 'background: #222; color: #ffff00; padding: 10px;');
-window.open("prime.html")
+window.open("prime.html", "_self")
 window.close();
 function check(){
 	input = document.getElementById("input");
