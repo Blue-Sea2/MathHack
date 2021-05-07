d163b7558b3a1ea2cccc0510b85abacea041e101
Max <65962153+Blue-Sea2@users.noreply.github.com>
2021-05-07 09:18:28 +0200

    Update prime.js

--- prime.js
+++ prime.js
@@ -1,6 +1,6 @@
 console.log('%c This is the prime number checker ', 'background: #222; color: #ffff00; padding: 10px;');
-var prime = new Audio("https://github.com/Blue-Sea2/MathHack/releases/download/Sounds/done.mp3");
-var notPrime = new Audio("https://github.com/Blue-Sea2/MathHack/releases/download/Sounds/done.mp3");
+var prime = new Audio("https://github.com/Blue-Sea2/MathHack/releases/download/Sounds/win.mp3");
+var notPrime = new Audio("https://github.com/Blue-Sea2/MathHack/releases/download/Sounds/lose.mp3");
 function check(){
 	input = document.getElementById("input");
 	output = document.getElementById("output");
